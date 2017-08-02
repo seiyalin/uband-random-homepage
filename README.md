6f693515599935611d30a18406c2f1e291eca4a5
seiyalin <seiyalin@airmind.co>
2017-08-02 14:23:58 +0800

    random homepage init

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # uband-random-homepage
 team9-random主页
+
+## 背景
+用Python的flask和简单的html，css做一个小组主页~~
+
+
+## 需求
+
+
+## 发布
+
+
+## 其他
+
+
+## 参考资料
---
+++ run.py
@@ -0,0 +1,15 @@
+from flask import Flask
+from flask import render_template
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return 'Hello World!'
+
+@app.route('/B17423')
+def hello_seiya():
+    return render_template('B17423.html')
+
+if __name__ == '__main__':
+	app.debug = True
+	app.run()
